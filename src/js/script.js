45ddd27de5ac7f71704b86692556000f9cebaa67
UglyMab <mabdev@ya.ru>
2022-08-26 15:50:59 +0300

    add markup and sass

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+const selects = document.querySelectorAll(".select-placeholder");
 const headerLogo = document.querySelector(".header-logo-link");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 10) {
@@ -22,9 +23,70 @@ document.addEventListener("click", (e) => {
   if (e.target != mobileMenuBtn && e.target != mobileMenuImage) {
     mobileMenu.classList.add("hidden");
   }
+  if (
+    e.target.classList.contains("select-item") ||
+    !e.target.classList.contains("select-placeholder")
+  ) {
+    selects.forEach((select) => {
+      const list = select.nextElementSibling;
+      list.classList.add("hidden");
+    });
+  }
 });
 const topBtn = document.querySelector(".footer-top-button");
 
 topBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
+});
+
+selects.forEach((select) => {
+  const list = select.nextElementSibling;
+  const input = select.previousElementSibling;
+  list.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+    select.innerText = value;
+    input.value = value;
+  });
+  select.addEventListener("click", (e) => {
+    list.classList.remove("hidden");
+  });
+});
+const buttons = document.querySelectorAll(".products-item__buttons");
+
+buttons.forEach((elem) => {
+  const minus = elem.querySelector(".button-minus");
+  const result = elem.querySelector(".result");
+  const plus = elem.querySelector(".button-plus");
+  const count = +result.dataset.count;
+
+  minus.addEventListener("click", () => {
+    if (+result.innerText > count) {
+      result.innerText = +result.innerText - count;
+    }
+  });
+  plus.addEventListener("click", () => {
+    result.innerText = +result.innerText + count;
+  });
+});
+const deliveryOpen = document.querySelector("#deliveryOpen");
+const cartOpen = document.querySelector("#cartOpen");
+
+cartOpen.addEventListener("click", () => {
+  document.querySelectorAll(".main-inner").forEach((elem) => {
+    if (!elem.classList.contains("cart-inner")) {
+      elem.classList.add("hidden");
+    } else {
+      elem.classList.remove("hidden");
+    }
+  });
+});
+
+deliveryOpen.addEventListener("click", () => {
+  document.querySelectorAll(".main-inner").forEach((elem) => {
+    if (!elem.classList.contains("delivery-inner")) {
+      elem.classList.add("hidden");
+    } else {
+      elem.classList.remove("hidden");
+    }
+  });
 });
